8840ca4dc907a37ef07947fe5379ee6d6f3205a1
abelmuro <abelmuro93@gmail.com>
2024-11-16 10:39:02 -0800

    comit

--- src/Controllers/POST/Register.js
+++ src/Controllers/POST/Register.js
@@ -4,6 +4,8 @@ const {User} = require('../../Models/Models.js')
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 
+//this is where i left off
+
 router.post('/register', (req, res) => {
     const {email, password} = req.body;
     const hashedPassword = bcrypt.hashSync(password, 8);
