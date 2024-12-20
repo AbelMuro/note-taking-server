77aca707accd8ed5522db46a492ee9411669ab8b
AbelMuro <abelmuro93@gmail.com>
2024-12-19 16:37:24 -0800

    commit

--- functions/api.js
+++ functions/api.js
@@ -2,6 +2,6 @@ const express = require('express');
 const serverless = require('serverless-http'); 
 const app = express(); 
 
-app.get('/hello', (req, res) => { res.send('Hello World!'); }); 
+app.get('/hello', (req, res) => { res.status(200).send('Hello World!'); }); 
 
 module.exports = serverless(app);
