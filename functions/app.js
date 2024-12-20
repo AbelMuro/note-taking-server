e512204ba579b5fc1c723d0b251c55213bb5b925
AbelMuro <abelmuro93@gmail.com>
2024-12-20 15:13:01 -0800

    commit

--- functions/app.js
+++ functions/app.js
@@ -1,12 +1,4 @@
-const express = require('express'); 
 const serverless = require('serverless-http'); 
-const app = express(); 
-const router = express.Router();
+const app = require('../src/index.js'); 
 
-//i need to figure out why my other routes are not working here
-router.get('/', (req, res) => { 
-    res.status(200).send('Hello World!'); 
-}); 
-
-app.use("/.netlify/functions/app", router);
 module.exports.handler = serverless(app);
