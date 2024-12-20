a999177d598bceb344160499f84f62daff951012
AbelMuro <abelmuro93@gmail.com>
2024-12-20 15:28:08 -0800

    commit

--- functions/app.js
+++ functions/app.js
@@ -1,4 +1,12 @@
 const serverless = require('serverless-http'); 
 const app = require('../src/index.js'); 
+const express = require('express');
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.send("App is running..");
+});
+
+app.use("/.netlify/functions/app", router);
 
 module.exports.handler = serverless(app);
