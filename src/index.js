17be21f56b0e9bbcf3de85598eefaf13b29a7509
AbelMuro <abelmuro93@gmail.com>
2024-12-20 16:09:06 -0800

    commit

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ app.use(cors({
 }));
 app.use(cookieParser())
 app.use('/.netlify/functions/app', register);
-app.use('/.netlify/functions/app', login);
+app.use('/.netlify/functions/app/login', login);
 app.use('/.netlify/functions/app', sendLink);
 app.use('/.netlify/functions/app', resetPassword);
 app.use('/.netlify/functions/app', account);
