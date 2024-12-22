58a36aba21027a0b8efaacf5654431ca38e45cda
abelmuro <abelmuro93@gmail.com>
2024-12-22 00:01:07 -0800

    coimmit

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ connectDB();
 
 app.use(express.json());
 app.use(cors({
-    origin: ['https://note-taking-app-front-end.netlify.app', 'http://localhost:3000'],
+    origin: 'https://note-taking-app-front-end.netlify.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type'],
     credentials: true,
