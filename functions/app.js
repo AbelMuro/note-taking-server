6002362f32e846ccbf47e5e6c4c3683fd702835f
abelmuro <abelmuro93@gmail.com>
2024-12-21 23:40:56 -0800

    commit

--- functions/app.js
+++ functions/app.js
@@ -1,8 +1,10 @@
 const serverless = require('serverless-http'); 
 const app = require('../src/index.js'); 
+const connectDB = require('../src/Database/db.js');
 
 const handler = serverless(app);
 module.exports.handler = async (e, context) => {
+    await connectDB();
     const result = await handler(e, context);
     return result;
 };
